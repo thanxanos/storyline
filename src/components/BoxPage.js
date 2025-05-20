60550a121cce090884333d797e960cfc9fd54282
thanxanos <zxanos@gmail.com>
2025-05-19 22:13:30 -0700

    book box page

--- src/components/BoxPage.js
+++ src/components/BoxPage.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import '../css/boxpage.css'
+import bookImg from '../imgFolder/bookboximg.jpg'
 
 function BoxPage() {
   return (
     <div id="top">
-      <h3>work in progress</h3>
-      <div id="fantasy">Fantasy</div>
-      <div id="romance">Romance</div>
-      <div id="science-fiction">Science Fiction</div> 
+      <div className='bookbox-wrapper'>
+        <div className='bookbox-desc'>
+          <img className="bookbox" alt='book-example' src={bookImg} />
+          <p>At Storyline, our goal is to deliver high quality, newly designed books to our subscribers. You can choose between one book box or all three! Miss a previous release? Our shop page has all prior released books available to purchase.</p>
+        </div>
+        <div><h1>Book Box Subscriptions</h1></div>
+        <div id="fantasy">Fantasy</div>
+        <div id="romance">Romance</div>
+        <div id="science-fiction">Science Fiction</div> 
+      </div>
     </div>
   )
 }
