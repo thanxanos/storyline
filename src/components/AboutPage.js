f86a8006de8bacb53af5dde1f59d8cd0c823d2af
thanxanos <zxanos@gmail.com>
2025-05-20 21:37:48 -0700

    updates

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,16 +5,16 @@ const AboutPage = () => {
   return (
     <div className='about-wrapper'>
       <div id='writers'>
-        <div className='writer-div'><h2>Our Writers</h2></div>
-        <div className='writer-p'><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Lorem auctor ac platea leo hendrerit cursus. Pretium accumsan vivamus montes facilisi ut turpis erat semper. Dis dolor vitae turpis rutrum montes sem quisque. Nascetur taciti est lacus; primis torquent litora.</p></div>
+        <div data-aos="fade-down" delay="200" data-aos-duration="1000" className='writer-div'><h2>Our Writers</h2></div>
+        <div data-aos="fade-down" delay="5" data-aos-duration="2500" className='writer-p'><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Lorem auctor ac platea leo hendrerit cursus. Pretium accumsan vivamus montes facilisi ut turpis erat semper. Dis dolor vitae turpis rutrum montes sem quisque. Nascetur taciti est lacus; primis torquent litora.</p></div>
       </div>
       <div id='artists'>
-        <div className='artist-p'><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Lorem auctor ac platea leo hendrerit cursus. Pretium accumsan vivamus montes facilisi ut turpis erat semper. Dis dolor vitae turpis rutrum montes sem quisque. Nascetur taciti est lacus; primis torquent litora.</p></div>
-        <div className='artist-div'><h2>Our Artists</h2></div>
+        <div data-aos="fade-down" delay="5" data-aos-duration="2500" className='artist-p'><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Lorem auctor ac platea leo hendrerit cursus. Pretium accumsan vivamus montes facilisi ut turpis erat semper. Dis dolor vitae turpis rutrum montes sem quisque. Nascetur taciti est lacus; primis torquent litora.</p></div>
+        <div data-aos="fade-down" delay="200" data-aos-duration="1000" className='artist-div'><h2>Our Artists</h2></div>
       </div>
       <div id='writers'>
-        <div className='writer-div'><h2>Our Quality</h2></div>
-        <div className='writer-p'><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Lorem auctor ac platea leo hendrerit cursus. Pretium accumsan vivamus montes facilisi ut turpis erat semper. Dis dolor vitae turpis rutrum montes sem quisque. Nascetur taciti est lacus; primis torquent litora.</p></div>
+        <div data-aos="fade-down" delay="200" data-aos-duration="1000" className='writer-div'><h2>Our Quality</h2></div>
+        <div data-aos="fade-down" delay="5" data-aos-duration="2500" className='writer-p'><p>Lorem ipsum odor amet, consectetuer adipiscing elit. Lorem auctor ac platea leo hendrerit cursus. Pretium accumsan vivamus montes facilisi ut turpis erat semper. Dis dolor vitae turpis rutrum montes sem quisque. Nascetur taciti est lacus; primis torquent litora.</p></div>
       </div>
     </div>
   )
--- src/components/BoxPage.js
+++ src/components/BoxPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../css/boxpage.css'
 import bookImg from '../imgFolder/bookboximg.jpg'
 
+
 function BoxPage() {
   return (
     <div id="top">
@@ -11,9 +12,29 @@ function BoxPage() {
           <p>At Storyline, our goal is to deliver high quality, newly designed books to our subscribers. You can choose between one book box or all three! Miss a previous release? Our shop page has all prior released books available to purchase.</p>
         </div>
         <div><h1>Book Box Subscriptions</h1></div>
-        <div id="fantasy">Fantasy</div>
-        <div id="romance">Romance</div>
-        <div id="science-fiction">Science Fiction</div> 
+        <div className="box-wrapper">
+          <div id="fantasy">
+            <div className="box-desc">
+              <h3>Fantasy</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor varius lobortis.</p>
+              <button className="primary-btn">Add to cart</button>
+            </div>
+          </div>
+          <div id="romance">
+            <div className="box-desc">
+              <h3>Romance</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor varius lobortis.</p>
+              <button className="primary-btn">Add to cart</button>
+            </div>
+          </div>
+          <div id="science-fiction">
+            <div className="box-desc">
+              <h3>Science Fiction</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor varius lobortis.</p>
+              <button className="primary-btn">Add to cart</button>
+            </div>
+          </div> 
+        </div>
       </div>
     </div>
   )
