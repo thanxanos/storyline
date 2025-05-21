b69df2c4cdb663b4d5ae2ef35a8d65ffc89dd317
thanxanos <zxanos@gmail.com>
2025-05-20 21:57:54 -0700

    final css for mvp

--- src/components/BoxPage.js
+++ src/components/BoxPage.js
@@ -7,27 +7,27 @@ function BoxPage() {
   return (
     <div id="top">
       <div className='bookbox-wrapper'>
-        <div className='bookbox-desc'>
+        <div data-aos="fade-up" delay="150" data-aos-duration="1200" className='bookbox-desc'>
           <img className="bookbox" alt='book-example' src={bookImg} />
           <p>At Storyline, our goal is to deliver high quality, newly designed books to our subscribers. You can choose between one book box or all three! Miss a previous release? Our shop page has all prior released books available to purchase.</p>
         </div>
         <div><h1>Book Box Subscriptions</h1></div>
         <div className="box-wrapper">
-          <div id="fantasy">
+          <div data-aos="fade-down" delay="200" data-aos-duration="1000" id="fantasy">
             <div className="box-desc">
               <h3>Fantasy</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor varius lobortis.</p>
               <button className="primary-btn">Add to cart</button>
             </div>
           </div>
-          <div id="romance">
+          <div data-aos="fade-down" delay="200" data-aos-duration="1000" id="romance">
             <div className="box-desc">
               <h3>Romance</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor varius lobortis.</p>
               <button className="primary-btn">Add to cart</button>
             </div>
           </div>
-          <div id="science-fiction">
+          <div data-aos="fade-down" delay="200" data-aos-duration="1000" id="science-fiction">
             <div className="box-desc">
               <h3>Science Fiction</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor varius lobortis.</p>
--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -40,9 +40,9 @@ const ContactPage = () => {
     <div className="contact-wrapper">
       <div className='accordion-wrapper'>
         <div className='accordion' id="faq">
-          <h2>Common Questions</h2>
+          <h2 data-aos="fade-up" delay="80" data-aos-duration="1050">Common Questions</h2>
             {data.map((item, i)=>
-              <div className='item'>
+              <div data-aos="fade-up" delay="150" data-aos-duration="1200" className='item'>
                 <div className='title' onClick={() => toggle(i)}>
                   <h3>{item.about}</h3>
                   <span>{selected === i ? '-' : '+'}</span>
@@ -54,22 +54,24 @@ const ContactPage = () => {
           </div>      
         </div>
         <div id="customer-service">
-          <h2>Customer Support</h2>
-          <div className='customer-service-container'>
+          <h2 data-aos="fade-up" delay="80" data-aos-duration="1050">Customer Support</h2>
+          <div data-aos="fade-up" delay="150" data-aos-duration="1200" className='customer-service-container'>
             <div className='cs-child'><FontAwesomeIcon icon={faPhone} className='contact-icon' /><h3>Call us</h3><p>1-800-000-1234</p></div>
             <div className='cs-child'><FontAwesomeIcon icon={faEnvelope} className='contact-icon' /><h3>Email us</h3><p>storyline@email.com</p></div>
             <div className='cs-child'><FontAwesomeIcon icon={faLocationDot} className='contact-icon' /><h3>Visit us</h3><p>1198 Cambridge Place</p><p>Edgewood, MD</p></div>
           </div>
           </div>
-        <img className='logo-img' alt='book-logo-image' src={logoImg} />
+        <img data-aos="fade-up" delay="80" data-aos-duration="1050" className='logo-img' alt='book-logo-image' src={logoImg} />
         <div id="tos">
-          <h2>Terms of Service</h2>
-          <p><b>OVERVIEW</b></p>
-          <p>Lorem ipsum odor amet, consectetuer adipiscing elit. At gravida sed class malesuada, volutpat hendrerit nascetur donec! Purus fringilla nisl nec varius nisl magnis. Fringilla aliquam semper rhoncus tempus purus vel curabitur. Est primis phasellus pulvinar erat euismod curae nunc? Rutrum a bibendum ad quisque mauris potenti nam aliquet.</p>
-          <p>Platea ac litora orci odio malesuada condimentum. Efficitur volutpat sem venenatis convallis cras suscipit dignissim erat dapibus. Consequat eget natoque viverra; interdum turpis semper cursus augue. Ipsum habitasse scelerisque vehicula est elementum sociosqu nulla. In bibendum aptent potenti convallis tempus amet. Massa sodales consequat hendrerit consectetur, morbi id velit penatibus convallis. Adipiscing efficitur imperdiet purus felis himenaeos fermentum platea.</p>
-          <p>Elementum ex sit ultrices primis; quam scelerisque. Arcu penatibus elementum netus praesent magnis odio blandit nisi. Ornare imperdiet lacus finibus fringilla diam dis. Potenti dignissim risus rutrum commodo pulvinar netus. Class cursus penatibus maximus consectetur, viverra vulputate. Sapien mauris a quam ridiculus vestibulum et quisque quis. Quis placerat tincidunt habitasse interdum, venenatis nunc ultricies parturient taciti. Penatibus natoque taciti congue, semper urna interdum natoque. Luctus aliquam metus cursus euismod maximus ornare felis sed gravida. Conubia dignissim platea porta orci bibendum netus.</p>
-          <p>Metus fames non est quisque nunc mauris eleifend maximus. Tristique nisl integer duis mi lacus risus porttitor. Sit turpis scelerisque volutpat dapibus dictum urna viverra dui. Mattis mus dui velit; accumsan rutrum semper? Justo nostra porttitor eros enim mus ac cursus metus in. Mollis donec accumsan lobortis aptent pellentesque. Aenean massa placerat sed augue eros pulvinar tempus quam. Venenatis fermentum sem parturient laoreet vulputate ante erat.</p>
-          <p>Aptent pulvinar rhoncus nam maecenas donec, integer curabitur. Condimentum mattis nunc cubilia aliquet nec nulla ornare. Purus nullam faucibus ante malesuada mi tempor eros. Fermentum tempor suspendisse bibendum efficitur fringilla sagittis lacinia consequat turpis. Congue maximus posuere diam viverra et nostra est. Interdum erat vehicula quis sollicitudin; sem aliquam. Tincidunt tortor fringilla molestie auctor facilisi vehicula euismod ac! Quis egestas nec lacus quisque natoque donec natoque auctor.</p>
+          <h2 data-aos="fade-up" delay="80" data-aos-duration="1050">Terms of Service</h2>
+          <p data-aos="fade-up" delay="150" data-aos-duration="1200"><b>OVERVIEW</b></p>
+            <div data-aos="fade-up" delay="150" data-aos-duration="1200">
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. At gravida sed class malesuada, volutpat hendrerit nascetur donec! Purus fringilla nisl nec varius nisl magnis. Fringilla aliquam semper rhoncus tempus purus vel curabitur. Est primis phasellus pulvinar erat euismod curae nunc? Rutrum a bibendum ad quisque mauris potenti nam aliquet.</p>
+            <p>Platea ac litora orci odio malesuada condimentum. Efficitur volutpat sem venenatis convallis cras suscipit dignissim erat dapibus. Consequat eget natoque viverra; interdum turpis semper cursus augue. Ipsum habitasse scelerisque vehicula est elementum sociosqu nulla. In bibendum aptent potenti convallis tempus amet. Massa sodales consequat hendrerit consectetur, morbi id velit penatibus convallis. Adipiscing efficitur imperdiet purus felis himenaeos fermentum platea.</p>
+            <p>Elementum ex sit ultrices primis; quam scelerisque. Arcu penatibus elementum netus praesent magnis odio blandit nisi. Ornare imperdiet lacus finibus fringilla diam dis. Potenti dignissim risus rutrum commodo pulvinar netus. Class cursus penatibus maximus consectetur, viverra vulputate. Sapien mauris a quam ridiculus vestibulum et quisque quis. Quis placerat tincidunt habitasse interdum, venenatis nunc ultricies parturient taciti. Penatibus natoque taciti congue, semper urna interdum natoque. Luctus aliquam metus cursus euismod maximus ornare felis sed gravida. Conubia dignissim platea porta orci bibendum netus.</p>
+            <p>Metus fames non est quisque nunc mauris eleifend maximus. Tristique nisl integer duis mi lacus risus porttitor. Sit turpis scelerisque volutpat dapibus dictum urna viverra dui. Mattis mus dui velit; accumsan rutrum semper? Justo nostra porttitor eros enim mus ac cursus metus in. Mollis donec accumsan lobortis aptent pellentesque. Aenean massa placerat sed augue eros pulvinar tempus quam. Venenatis fermentum sem parturient laoreet vulputate ante erat.</p>
+            <p>Aptent pulvinar rhoncus nam maecenas donec, integer curabitur. Condimentum mattis nunc cubilia aliquet nec nulla ornare. Purus nullam faucibus ante malesuada mi tempor eros. Fermentum tempor suspendisse bibendum efficitur fringilla sagittis lacinia consequat turpis. Congue maximus posuere diam viverra et nostra est. Interdum erat vehicula quis sollicitudin; sem aliquam. Tincidunt tortor fringilla molestie auctor facilisi vehicula euismod ac! Quis egestas nec lacus quisque natoque donec natoque auctor.</p>
+            </div>
           </div>
       </div>
   )
